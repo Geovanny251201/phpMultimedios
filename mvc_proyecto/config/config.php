39a6050358f7d0711037807509faef49b7d99bb5
Geovanny251201 <69869435+Geovanny251201@users.noreply.github.com>
2023-06-03 18:03:33 -0600

    cambio

---
+++ mvc_proyecto/config/config.php
@@ -0,0 +1,14 @@
+<?php 
+define('HOST','sql863.main-hosting.eu');
+define('DB','u484426513_apireact');
+define('USER','u484426513_apireact');
+define('PASSWORD','i:![VW:3S#');
+define('CHARSET', 'utf8mb4');
+
+
+//url del proyecto web
+
+
+
+
+?>
